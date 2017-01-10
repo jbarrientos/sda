abb7e64b7c4b2d51539a7bf2e003a2302b49a881
jbarr <jbarr@LAPTOP-176ODVD6>
2017-01-10 11:46:28 -0600

    Add project files.

---
+++ SDA.WebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SDA.WebApp.Startup))]
+namespace SDA.WebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
